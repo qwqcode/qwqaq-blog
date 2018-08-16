b6cc2daefae1b7b9cda565c166e47a574f0c454a
qwqcode <1149527164@qq.com>
2018-08-16 22:13:20 +0800

    Site updated: 2018-08-16 22:13:04

--- js/main.js
+++ js/main.js
@@ -32,6 +32,53 @@
         $('.justified-gallery').justifiedGallery(options);
     }
 
+    // Article content index
+    (function () {
+        var indexElem = $('#article-content-index');
+        var contentElem = $('.article-entry');
+
+        if (!indexElem.length || !contentElem.length)
+            return;
+
+        var indexListElem = indexElem.find('.index-list');
+        indexListElem.css("max-height", ($(window).height() - $('#profile')[0].offsetTop - $('#profile').height() - 200) + "px");
+        var titles = contentElem.find('h2, h3, h4, h5, h6');
+        if (!titles.length)
+            return;
+        titles.each(function (i, elem) {
+            var titleHref = $(elem).find('a').attr('href');
+            var titleText = $(elem).text();
+            var tagNum = Number($(elem)[0].tagName.substr(1));
+            var itemElem = $('<div class="item type-'+tagNum+'" data-key="'+i+'"><a href="'+titleHref+'">'+titleText+'</a></div>');
+            itemElem.appendTo(indexListElem);
+        });
+
+        indexElem.show();
+
+        var selectedKey = null;
+        var checkAndSelect = function () {
+            titles.each(function (i, elem) {
+                var isCanSee = elem.offsetTop >= $(window).scrollTop() && elem.offsetTop < ($(window).scrollTop() + $(window).height());
+                if (isCanSee) {
+                    if (i !== selectedKey) {
+                        selectedKey = i;
+                        indexListElem.find('.active').removeClass('active');
+                        var itemElem = indexListElem.find('.item[data-key="'+i+'"]');
+                        itemElem.addClass('active');
+                        indexListElem.scrollTop(itemElem[0].offsetTop - itemElem.height() * 2);
+                    }
+                    return false;
+                }
+                return true;
+            });
+        };
+
+        checkAndSelect();
+        $(document).on('scroll', function () {
+            checkAndSelect();
+        });
+    })();
+
     // Profile card
     $(document).on('click', function () {
         $('#profile').removeClass('card');
@@ -46,45 +93,47 @@
      * To Top & Fixed Profile
      */
     if ($('#sidebar').length) {
-        checkFixedProfile();
+        checkSidebarLeftFixed();
         checkShowToTop();
 
         $(document).on('scroll', function () {
-            checkFixedProfile();
+            checkSidebarLeftFixed();
             checkShowToTop();
-        }).on('click', '#toTop', function () {
+        });
+        
+        $('#toTop').click(function () {
             $('body, html').animate({ scrollTop: 0 }, 600);
         });
     }
 
-    function checkFixedProfile() {
+    function checkSidebarLeftFixed() {
         var scrollTop = $(document).scrollTop();
-        var profileElem = $('#profile');
-        var profileInnerElem = $('#profile .profile-inner');
-        var needFixedProfile = scrollTop >= profileElem.offset().top + profileElem.outerHeight(true);
-        var isProfileFixed = profileInnerElem.is('.profile-fixed');
+        var fixedElem = $('#sidebar-left .sidebar-fixed');
+        var InnerElem = $('#sidebar-left .sidebar-fixed .sidebar-fixed-inner');
+        var needFixedElem = scrollTop >= fixedElem.offset().top + fixedElem.outerHeight(true);
+        var isFixed = InnerElem.is('.has-fixed');
 
-        if (!needFixedProfile) {
+        if (!needFixedElem) {
             // 不固定
-            if (!isProfileFixed) return;
+            if (!isFixed) return;
 
-            profileInnerElem.removeClass('profile-fixed');
-            profileInnerElem.css('position', '')
+            InnerElem.removeClass('has-fixed');
+            InnerElem.css('position', '')
                 .css('width', '')
                 .css('top', '');
 
-            profileInnerElem.css('animation', 'none');
+                InnerElem.css('animation', 'none');
         } else {
             // 需固定
-            if (isProfileFixed) return;
+            if (isFixed) return;
 
-            profileInnerElem.addClass('profile-fixed');
-            profileInnerElem.css('position', 'fixed')
-                .css('width', profileElem.innerWidth() + 'px')
+            InnerElem.addClass('has-fixed');
+            InnerElem.css('position', 'fixed')
+                .css('width', fixedElem.innerWidth() + 'px')
                 .css('top', '0');
 
-            profileInnerElem.css('animation', '');
-            profileInnerElem.addClass('anim-fade-in');
+            InnerElem.css('animation', '');
+            InnerElem.addClass('anim-fade-in');
         }
     }
 
